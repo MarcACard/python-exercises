c07eb679072f264974985d76febdbc9514291042
Marc Card <marcacard@gmail.com>
2021-05-23 16:34:53 -0700

    add 18.4 python OOP exercises

---
+++ oop/serial.py
@@ -0,0 +1,22 @@
+"""Python serial number generator."""
+
+class SerialGenerator:
+    """Machine to create unique incrementing serial numbers.
+    
+    >>> serial = SerialGenerator(start=100)
+
+    >>> serial.generate()
+    100
+
+    >>> serial.generate()
+    101
+
+    >>> serial.generate()
+    102
+
+    >>> serial.reset()
+
+    >>> serial.generate()
+    100
+    """
+
