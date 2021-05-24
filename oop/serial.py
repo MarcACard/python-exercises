6b29e18f46b2d8763cc90ba39d748ae93f8169b3
Marc Card <marcacard@gmail.com>
2021-05-23 19:36:42 -0700

    update 18.4 python OOP Exercises with Solutions

--- oop/serial.py
+++ oop/serial.py
@@ -1,8 +1,9 @@
 """Python serial number generator."""
 
+
 class SerialGenerator:
     """Machine to create unique incrementing serial numbers.
-    
+
     >>> serial = SerialGenerator(start=100)
 
     >>> serial.generate()
@@ -20,3 +21,20 @@ class SerialGenerator:
     100
     """
 
+    def __init__(self, start):
+        self.start = start
+        self.next = start
+
+    def __repr__(self):
+        return f"<SerialGenerator start={self.start} next={self.next}>"
+
+    def generate(self):
+        """Return a serial # and intrement next_serial attribute"""
+        serial = self.next
+        self.next += 1
+
+        return serial
+
+    def reset(self):
+        """Reset the next_serial to the initialized starting value"""
+        self.next = self.start
--- oop/wordfinder.py
+++ oop/wordfinder.py
@@ -1,5 +1,43 @@
 """Word Finder: finds random words from a dictionary."""
 
+from random import choice
+
 
 class WordFinder:
-    ...
+    """A class to process and return random words from a file"""
+
+    def __init__(self, file_path):
+        f = open(file_path)
+        self.words = self.parse_file(f)
+        self.source = file_path
+
+        print(f"{len(self.words)} words read")
+
+    def __repr__(self):
+        return f"<WordFinder word_count={len(self.words)} source={self.source}>"
+
+    def parse_file(self, f):
+        """Parse a file and return a list containing each line."""
+        return [line.rstrip() for line in f]
+
+    def random(self):
+        """Return a random word from the words attribute"""
+        return choice(self.words)
+
+
+class SpecialWordFinder(WordFinder):
+    """A special WordFinder that is able to process files with blank lines & comments"""
+
+    def __init__(self, file_path):
+        super().__init__(file_path)
+
+    def __repr__(self):
+        return f"<SpecialWordFinder word_count={len(self.words)} source={self.source}>"
+
+    def parse_file(self, f):
+        """Parse a file and return a list with each line.Ignore empty lines & \\n characters"""
+        return [
+            line.rstrip()
+            for line in f
+            if not line.startswith("#") and not line.startswith("\n")
+        ]
