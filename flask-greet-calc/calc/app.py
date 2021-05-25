082f175dc5ce84cb4e33184006ff1d3c6080e363
Marc Card <marcacard@gmail.com>
2021-05-24 21:53:05 -0700

    update 19.1 calc app with partial solution (before further study)

--- flask-greet-calc/calc/app.py
+++ flask-greet-calc/calc/app.py
@@ -1,1 +1,36 @@
-# Put your app in here.
+from flask import Flask, request
+from operations import *
+
+app = Flask(__name__)
+
+
+@app.route("/add")
+def get_add():
+    a = int(request.args.get("a", ""))
+    b = int(request.args.get("b", ""))
+
+    return str(add(a, b))
+
+
+@app.route("/sub")
+def get_sub():
+    a = int(request.args.get("a", ""))
+    b = int(request.args.get("b", ""))
+
+    return str(sub(a, b))
+
+
+@app.route("/mult")
+def get_mult():
+    a = int(request.args.get("a", ""))
+    b = int(request.args.get("b", ""))
+
+    return str(mult(a, b))
+
+
+@app.route("/div")
+def get_div():
+    a = int(request.args.get("a", ""))
+    b = int(request.args.get("b", ""))
+
+    return str(div(a, b))
